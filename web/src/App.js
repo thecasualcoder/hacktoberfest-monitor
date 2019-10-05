42f27ef8d20fcc3ef343d4b15b6281d49ad8bf18
Krishnaswamy Subramanian <jskswamy@gmail.com>
2019-10-05 11:49:14 +0530

    Add alert to notify success and failure

--- web/src/App.js
+++ web/src/App.js
@@ -1,42 +1,40 @@
 import React from 'react';
+import {useAlert} from 'react-alert';
 import Form from "react-jsonschema-form";
 import axios from 'axios';
 import './App.css';
 
 const languages = [
-  "GoLang", "Java", "Python", "Kotlin", "Swift", "Objective C", "C#", "JavaScript", "Ruby", "Shell Script", "TypeScript", "CSS", "C++", "Rust", "Other"
-].sort()
+    "GoLang", "Java", "Python", "Kotlin", "Swift", "Objective C", "C#", "JavaScript", "Ruby", "Shell Script", "TypeScript", "CSS", "C++", "Rust", "Other"
+].sort();
 
 const schema = {
-  title: "Hacktoberfest - ThoughtWorks Chennai",
-  type: "object",
-  required: ["pr_link", "language"],
-  properties: {
-    pr_link: { type: "string", title: "Pull Request Link" },
-    language: { type: "string", title: "Language", "enum": languages }
-  }
+    title: "Hacktoberfest - ThoughtWorks Chennai",
+    type: "object",
+    required: ["pr_link", "language"],
+    properties: {
+        pr_link: {type: "string", title: "Pull Request Link"},
+        language: {type: "string", title: "Language", "enum": languages}
+    }
 };
 
 const log = (type) => console.log.bind(console, type);
 
 function App() {
-
-  return (
-    <Form schema={schema}
-      onChange={log("changed")}
-      onSubmit={(data) => {
-        console.log(data.formData)
-
-        axios.post('/api/pr', data.formData)
-          .then(function (response) {
-            console.log(response);
-          })
-          .catch(function (response) {
-            console.log(response);
-          });
-      }}
-      onError={log("errors")} />
-  );
+    const alert = useAlert();
+    return (
+        <Form schema={schema}
+              onSubmit={async (data) => {
+                  console.log(data.formData);
+                  try {
+                      await axios.post('/api/pr', data.formData);
+                      alert.show("Thanks for your contribution");
+                  } catch (err) {
+                      alert.show("Unable to save your contribution, please contact the volunteer");
+                  }
+              }}
+              onError={log("errors")}/>
+    );
 }
 
 export default App;
--- web/src/index.js
+++ web/src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {transitions, positions, Provider as AlertProvider} from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const alertOptions = {
+    position: positions.TOP_CENTER,
+    timeout: 5000,
+    offset: '30px',
+    transition: transitions.SCALE,
+    width: '600px',
+};
+
+const Root = () => (
+    <AlertProvider template={AlertTemplate} {...alertOptions}>
+        <App/>
+    </AlertProvider>
+);
+
+ReactDOM.render(Root(), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
