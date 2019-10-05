c1d291fb0093ef95978dbebf7b1e66cdadc849f8
Krishnaswamy Subramanian <jskswamy@gmail.com>
2019-10-05 11:52:23 +0530

    Format api nodejs file

--- api/index.js
+++ api/index.js
@@ -1,35 +1,35 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const rateLimit = require("express-rate-limit")
-const axios = require('axios')
-const app = express()
-const port = 3000
+const express = require('express');
+const bodyParser = require('body-parser');
+const rateLimit = require("express-rate-limit");
+const axios = require('axios');
+const app = express();
+const port = 3000;
 
 const limiter = rateLimit({
     windowMs: 1 * 60 * 1000, // 15 minutes
     max: 60 // limit each IP to 100 requests per windowMs
-})
+});
 
-app.use(limiter)
-app.use(bodyParser.json())
+app.use(limiter);
+app.use(bodyParser.json());
 
-const dbUrl = process.env.DB_URL || "http://influxdb:8086"
+const dbUrl = process.env.DB_URL || "http://influxdb:8086";
 
-app.get('/ping', (req, res) => res.json({ "message": "pong" }))
+app.get('/ping', (req, res) => res.json({"message": "pong"}));
 
 app.post('/api/pr', async (req, res) => {
-    console.log(req.body)
-    const { pr_link: prLink, language } = req.body
+    console.log(req.body);
+    const {pr_link: prLink, language} = req.body;
 
     if (!prLink || !language) {
-        res.status(400).json({ "message": "wrong data" })
+        res.status(400).json({"message": "wrong data"});
         return
     }
 
     try {
-        const prUrl = new URL(prLink)
-        if(prUrl.host !== "github.com") {
-            throw Error("not github.com")
+        const prUrl = new URL(prLink);
+        if (prUrl.host !== "github.com") {
+            throw Error("not github.com");
         }
 
         await axios({
@@ -37,24 +37,24 @@ app.post('/api/pr', async (req, res) => {
             url: prLink
         })
     } catch (e) {
-        res.status(400).json({ "message": "wrong PR URL" })
-        return
+        res.status(400).json({"message": "wrong PR URL"});
+        return;
     }
 
-    const body = `pull_request,pr_link=${prLink.trim().replace(/ +/g, "-")},language=${language.trim().replace(/ +/g, "-")} value=1`
+    const body = `pull_request,pr_link=${prLink.trim().replace(/ +/g, "-")},language=${language.trim().replace(/ +/g, "-")} value=1`;
 
-    const url = `${dbUrl}/write?db=hacktober_metrics`
-    console.log(`Sending request to: ${url}`)
+    const url = `${dbUrl}/write?db=hacktober_metrics`;
+    console.log(`Sending request to: ${url}`);
     try {
         await axios({
             method: 'post',
             url,
             data: body,
-        })
+        });
         res.sendStatus(204)
     } catch (e) {
         res.sendStatus(500)
     }
-})
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
