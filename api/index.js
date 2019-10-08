db5766cd01acc501709f621e600f7a54339c3d9f
Michael Eason <mike.eason@mail.com>
2019-10-08 20:15:40 +0100

    Add configurable port and fixed dotenv not loading environment variables

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const rateLimit = require("express-rate-limit");
 const axios = require('axios');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const limiter = rateLimit({
     windowMs: 1 * 60 * 1000, // 15 minutes
